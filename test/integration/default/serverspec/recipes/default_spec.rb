17830665fd46ace83e95334a25b2d2a04a00a524
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-10-09 08:51:04 -0700

    initial build

--- test/integration/default/serverspec/recipes/default_spec.rb
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2015 Nordstrom, Inc.
-#
-require 'spec_helper'
-
-describe command('test-path C:\\Windows\\Temp\\PrintQueueBackup.bak') do
-  its(:stdout) { should match(/^true/) }
-end
