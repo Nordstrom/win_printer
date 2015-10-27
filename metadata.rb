908131bd05418cfa23d54001199ade815eead384
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-10-27 11:37:50 -0700

    updated metadata.rb with source/issues urls

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,9 @@ maintainer       'EPG WSE Team, Nordstrom, Inc.'
 maintainer_email 'itwseall@nordstrom.com'
 license          'Apache v2.0'
 description      'Installs/Configures win_printer'
-version          '1.3.0'
+version          '1.3.1'
 
 depends 'wse_powershell_helper', '~> 0.1'
+
+source_url 'https://github.com/Nordstrom/win_printer'
+issues_url 'https://github.com/Nordstrom/win_printer/issues'
