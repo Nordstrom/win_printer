de02c9be63bca49e1255d8bc7cdbef61a8ec7613
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-10-27 12:18:52 -0700

    updated LICENSE

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,22 @@
+# Encoding: utf-8
+# Copyright 2015 Nordstrom, Inc.
 #
-# Copyright (c) 2015 Nordstrom, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 name             'win_printer'
 maintainer       'EPG WSE Team, Nordstrom, Inc.'
-maintainer_email 'itwseall@nordstrom.com'
 license          'Apache v2.0'
 description      'Installs/Configures win_printer'
 version          '1.3.1'
